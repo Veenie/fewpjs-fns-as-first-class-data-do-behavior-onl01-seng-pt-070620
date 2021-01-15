4117ab26fc4c1cb6f4d770e5840e4dace7416d89
Raveen K Sharma <raveen08@gmail.com>
2021-01-15 18:26:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ function handleClick(e) {
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
 
-function greet() {
-  
+function greet(time) {
+  let time = 
 }
